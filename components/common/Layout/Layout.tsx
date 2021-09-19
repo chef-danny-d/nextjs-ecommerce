d38a24b1c1f170a6b2b1ffec570e06d2503df1f1
Daniel Papp <dpapp001@odu.edu>
2021-09-19 19:39:07 -0400

    💄 (style): Added styling
    Added sass and tailwinds to the project

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react"
 
 const Layout: FC = ({ children }) => {
-	return <div className="layout">{children}</div>
+	return <div className="layout text-white">{children}</div>
 }
 
 export default Layout
--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
 const { withFrameworkConfig } = require("./framework/common/config")
 require("dotenv").config()
+const path = require("path")
 
 module.exports = withFrameworkConfig({
 	framework: {
 		name: process.env.FRAMEWORK,
 	},
 	i18n: {
-		locales: ["en-US", "es"],
+		locales: ["en-US"],
 		defaultLocale: "en-US",
 	},
 	reactStrictMode: true,
+	sassOptions: {
+		includePaths: [path.join(__dirname, "assets")],
+	},
 })
 
 console.log("⚙️   next.config.js", JSON.stringify(module.exports, null))
